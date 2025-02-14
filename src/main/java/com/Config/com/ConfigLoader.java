447a6aea48160258e9599d1ad72431e5280e4abc
Lakshminarayana <lakshminarayanas86@gmail.com>
2025-02-14 10:04:01 +0530

    code change to Supports multiple properties files dynamically configloader

--- src/main/java/com/Config/com/ConfigLoader.java
+++ src/main/java/com/Config/com/ConfigLoader.java
@@ -1,6 +1,9 @@
 package com.Config.com;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 import com.aventstack.extentreports.Status;
 import com.constants.com.FrameworkConstants;
@@ -8,36 +11,50 @@ import com.utils.ExtentReportManager;
 
 public class ConfigLoader extends FrameworkConstants {
 
+	private static final Map<String, Properties> propertiesMap = new HashMap<>();
+
 	static {
-		try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("config.properties")) {
+		loadProperties("config.properties");
+		loadProperties("data.properties"); // Load additional properties file
+	}
+
+	private static void loadProperties(String fileName) {
+		try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream(fileName)) {
 			if (input == null) {
-				String errorMessage = "Unable to find config.properties";
+				String errorMessage = "Unable to find " + fileName;
 				ExtentReportManager.log(Status.FAIL, errorMessage);
 				throw new RuntimeException(errorMessage);
 			}
 			// Load the properties file
 			properties.load(input);
-			ExtentReportManager.log(Status.INFO, "Successfully loaded config.properties");
+			propertiesMap.put(fileName, properties);
+			ExtentReportManager.log(Status.INFO, "Successfully loaded " + fileName);
 		} catch (Exception e) {
-			String errorMessage = "Error loading config.properties: " + e.getMessage();
+			String errorMessage = "Error loading " + fileName + ": " + e.getMessage();
 			ExtentReportManager.log(Status.FAIL, errorMessage);
 			System.err.println(errorMessage);
 			e.printStackTrace();
 		}
 	}
 
-	public static String getProperty(String key) {
+	public static String getProperty(String fileName, String key) {
+		Properties properties = propertiesMap.get(fileName);
+
+		if (properties == null) {
+			String errorMessage = "Properties file '" + fileName + "' not loaded.";
+			ExtentReportManager.log(Status.FAIL, errorMessage);
+			throw new RuntimeException(errorMessage);
+		}
 
 		String value = properties.getProperty(key);
 
 		if (value == null) {
-			String errorMessage = "Property key '" + key + "' not found in config file.";
+			String errorMessage = "Property key '" + key + "' not found in " + fileName;
 			ExtentReportManager.log(Status.WARNING, errorMessage);
 			throw new RuntimeException(errorMessage);
 		}
 
-		ExtentReportManager.log(Status.INFO, "Retrieved property: " + key + " = " + value);
+		ExtentReportManager.log(Status.INFO, "Retrieved property from " + fileName + ": " + key + " = " + value);
 		return value;
 	}
-
 }
--- src/main/java/com/constants/com/FrameworkConstants.java
+++ src/main/java/com/constants/com/FrameworkConstants.java
@@ -8,44 +8,45 @@ public class FrameworkConstants {
 
 	public static final Properties properties = new Properties();
 
-	public static final String BROWSER_NAME = ConfigLoader.getProperty("browser.name");
+	public static final String BROWSER_NAME = ConfigLoader.getProperty("config.properties", "browser.name");
 
-	public static final String BASE_URL = ConfigLoader.getProperty("base.url");
+	public static final String BASE_URL = ConfigLoader.getProperty("config.properties", "base.url");
 
-	public static final String DROPDOWN_TYPE = ConfigLoader.getProperty("dropdown.type");
+	public static final String DROPDOWN_TYPE = ConfigLoader.getProperty("config.properties", "dropdown.type");
 
-	public static final String SCREENSHOT_DIR = ConfigLoader.getProperty("screenshot.dir");
+	public static final String SCREENSHOT_DIR = ConfigLoader.getProperty("config.properties", "screenshot.dir");
 
-	public static final String FULL_NAME = ConfigLoader.getProperty("full.name");
+	public static final String FULL_NAME = ConfigLoader.getProperty("config.properties", "full.name");
 
-	public static final String EMAIL_ID = ConfigLoader.getProperty("email.id");
+	public static final String EMAIL_ID = ConfigLoader.getProperty("config.properties", "email.id");
 
-	public static final String COMPANY_NAME = ConfigLoader.getProperty("company.name");
+	public static final String COMPANY_NAME = ConfigLoader.getProperty("config.properties", "company.name");
 
-	public static final String PHONE_NO = ConfigLoader.getProperty("phone.no");
+	public static final String PHONE_NO = ConfigLoader.getProperty("config.properties", "phone.no");
 
-	public static final String PASS_WORD = ConfigLoader.getProperty("password");
+	public static final String PASS_WORD = ConfigLoader.getProperty("config.properties", "password");
 
-	public static final String CONFIRM_PASSWORD = ConfigLoader.getProperty("confirm.password");
+	public static final String CONFIRM_PASSWORD = ConfigLoader.getProperty("config.properties", "confirm.password");
 
-	public static final String OTP = ConfigLoader.getProperty("O.T.P");
+	public static final String OTP = ConfigLoader.getProperty("config.properties", "O.T.P");
 
-	public static final String JOB_TITLE = ConfigLoader.getProperty("job.title");
+	public static final String JOB_TITLE = ConfigLoader.getProperty("config.properties", "job.title");
 
-	public static final String JOB_SKILLS = ConfigLoader.getProperty("job.skills");
+	public static final String JOB_SKILLS = ConfigLoader.getProperty("config.properties", "job.skills");
 
-	public static final String JOB_LOCATION = ConfigLoader.getProperty("job.location");
+	public static final String JOB_LOCATION = ConfigLoader.getProperty("config.properties", "job.location");
 
-	public static final String JOB_TYPE = ConfigLoader.getProperty("job.type");
+	public static final String JOB_TYPE = ConfigLoader.getProperty("config.properties", "job.type");
 
-	public static final String EMPLOYEMENT_TYPE = ConfigLoader.getProperty("employement.type");
+	public static final String EMPLOYEMENT_TYPE = ConfigLoader.getProperty("config.properties", "employement.type");
 
-	public static final String EXPERIENCE_LEVEL = ConfigLoader.getProperty("experience.level");
+	public static final String EXPERIENCE_LEVEL = ConfigLoader.getProperty("config.properties", "experience.level");
 
-	public static final String SALARY_LEVEL = ConfigLoader.getProperty("salary.level");
+	public static final String SALARY_LEVEL = ConfigLoader.getProperty("config.properties", "salary.level");
 
-	public static final String LANGUAGE_PREFERENCE = ConfigLoader.getProperty("language.preference");
+	public static final String LANGUAGE_PREFERENCE = ConfigLoader.getProperty("config.properties",
+			"language.preference");
 
-	public static final String TONE = ConfigLoader.getProperty("tone");
+	public static final String TONE = ConfigLoader.getProperty("config.properties", "tone");
 
 }
